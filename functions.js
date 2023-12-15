5ee892e84d47e6eaaa03b40e2ee87dadeaebb439
Gonzalez Tenreiro <jgonzalezt@santiagodecompostela.gal>
2023-12-15 14:37:37 +0100

    functions.js

---
+++ functions.js
@@ -0,0 +1,36 @@
+// Calcular media de 3 números e irlos mostrando por pantalla
+
+// Imaginemos que los va introduciendo un usuario
+// Valores 3,5,6
+// Valores 1,8,5
+// Valores 2,3,5
+// Valores 3.3, 98, 32
+
+// Primera instancia
+// Declaración/inicialización de variables, cálculo y muestra de resultados
+let num1 = 3
+let num2 = 5
+let num3 = 6
+let promedio = num1 + num2 + num3 / 3
+console.log(promedio)
+
+// Segunda instancia
+num1 = 1
+num2 = 8
+num3 = 5
+promedio = num1 + num2 + num3 / 3
+console.log(promedio)
+
+// Tercera instancia
+num1 = 2
+num2 = 3
+num3 = 5
+promedio = num1 + num2 + num3 / 3
+console.log(promedio)
+
+// Cuarta instancia
+num1 = 3.3
+num2 = 98
+num3 = 32
+promedio = num1 + num2 + num3 / 3
+console.log(promedio)
